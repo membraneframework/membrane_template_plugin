131365913108d9fa1aca3c80c22c12183edeb3f5
sheldak <samuel.heldak@gmail.com>
2021-06-23 12:23:53 +0200

    update unifex version

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Membrane.Template.Mixfile do
   defp deps do
     [
       {:membrane_core, "~> 0.6.0"},
-      {:unifex, "~> 0.3.1"},
+      {:unifex, "~> 0.4.0"},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
       {:credo, "~> 1.4", only: :dev, runtime: false}
