0542504e96bc21213204af889dc27b2a18b605ba
Agnieszka Dutka <agnieszka.dutka@swmansion.com>
2021-12-03 16:08:04 +0100

    bump dependencies

--- mix.exs
+++ mix.exs
@@ -36,10 +36,10 @@ defmodule Membrane.Template.Mixfile do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.7.0"},
-      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+      {:membrane_core, "~> 0.8.1"},
+      {:ex_doc, "~> 0.26", only: :dev, runtime: false},
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
-      {:credo, "~> 1.5", only: :dev, runtime: false}
+      {:credo, "~> 1.6", only: :dev, runtime: false}
     ]
   end
 
